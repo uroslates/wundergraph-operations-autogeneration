edcb016271b34b62b982f9d6785b3201c5766780
Uros Lates <u.lates@sap.com>
2023-04-05 16:55:06 +0200

    feat(occ): add occ api spec with wg config

--- .wundergraph/wundergraph.config.ts
+++ .wundergraph/wundergraph.config.ts
@@ -7,9 +7,18 @@ const countries = introspect.graphql({
 	url: 'https://countries.trevorblades.com/',
 });
 
+const occ = introspect.openApi({
+	apiNamespace: 'occ',
+	source: {
+		kind: 'file',
+		filePath: '../specs/occ.json',
+	},
+	baseURL: 'https://spartacus-demo.eastus.cloudapp.azure.com:8443/occ/v2/',
+});
+
 // configureWunderGraph emits the configuration
 configureWunderGraphApplication({
-	apis: [countries],
+	apis: [countries, occ],
 	server,
 	operations,
 	codeGenerators: [
@@ -22,15 +31,18 @@ configureWunderGraphApplication({
 	],
 	cors: {
 		...cors.allowAll,
-		allowedOrigins:
-			process.env.NODE_ENV === 'production'
-				? [
-						// change this before deploying to production to the actual domain where you're deploying your app
-						'http://localhost:3000',
-				  ]
-				: ['http://localhost:3000', new EnvironmentVariable('WG_ALLOWED_ORIGIN')],
+		// allowedOrigins:
+		// 	process.env.NODE_ENV === 'production'
+		// 		? [
+		// 				// change this before deploying to production to the actual domain where you're deploying your app
+		// 				'http://localhost:3000',
+		// 		  ]
+		// 		: ['http://localhost:3000', new EnvironmentVariable('WG_ALLOWED_ORIGIN')],
+		allowedOrigins: ['http://localhost:3000','*'],
+		allowCredentials: true,
 	},
 	security: {
-		enableGraphQLEndpoint: process.env.NODE_ENV !== 'production' || process.env.GITPOD_WORKSPACE_ID !== undefined,
+		// enableGraphQLEndpoint: process.env.NODE_ENV !== 'production' || process.env.GITPOD_WORKSPACE_ID !== undefined,
+		enableGraphQLEndpoint: true,
 	},
 });
